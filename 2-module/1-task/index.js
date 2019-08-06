b8d7bff0f8ec636b3d99f1125c6ece8be428004f
Goodcat73 <smithwest73@gmail.com>
2019-08-06 18:47:02 +0300

    решение задач 1,2 во втором модуле

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,13 @@
  * @returns {Object}
  */
 function clone(obj) {
+  let copy = {};
+  for (let key in obj) {
+    if (obj[key] && typeof obj[key] === 'object') {
+      copy[key] = clone(obj[key]);
+    } else {
+      copy[key] = obj[key];
+    }
+  }
+  return copy;
 }
