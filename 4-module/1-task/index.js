2121f040abc450a7d535d04b12d4c0acc2bc5673
Goodcat73 <smithwest73@gmail.com>
2019-08-12 17:48:12 +0300

    решил задачу 1 в модуле 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,14 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+	let listOfFriends = ``;
+	let ul = document.createElement('ul');
+	document.body.append(ul);
+
+	for (let id of friends) {
+		listOfFriends += `<li>${id.firstName} ${id.lastName}</li>`;
+	}
+
+	ul.innerHTML = `${listOfFriends}`;
+	return ul;
 }
