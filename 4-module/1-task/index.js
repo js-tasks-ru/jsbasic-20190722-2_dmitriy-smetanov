7c27cfa5fe2e2a3d956168735d9c2d472278a5c9
Goodcat73 <smithwest73@gmail.com>
2019-08-15 21:21:46 +0300

    изменил решение в задаче  4.1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -3,15 +3,15 @@
  * @param {Object[]} friends
  * @return {HTMLUListElement}
  */
+
 function makeFriendsList(friends) {
-	let listOfFriends = ``;
-	let ul = document.createElement('ul');
-	document.body.append(ul);
+	let listOfFriends = '';
+	let ul = document.createElement('UL');
 
 	for (let id of friends) {
 		listOfFriends += `<li>${id.firstName} ${id.lastName}</li>`;
 	}
 
-	ul.innerHTML = `${listOfFriends}`;
+	ul.innerHTML = listOfFriends;
 	return ul;
 }
